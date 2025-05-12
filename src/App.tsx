d91dc98ca89cf196f96e75f3192081e4ceb4e645
mariadebahia <maria.zerihoun@student.berghs.se>
2025-05-12 09:12:28 +0200

    Update background color to white

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   }, []);
 
   return (
-    <div className="min-h-screen bg-gradient-to-b from-white to-gray-50">
+    <div className="min-h-screen bg-white">
       <HeroSection />
       <IntroSection />
       <QuizSection />
