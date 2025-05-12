8edaa718764bac3ccd7f6b0dfea0f241f392b133
mariadebahia <maria.zerihoun@student.berghs.se>
2025-05-12 09:12:57 +0200

    Update background color to #ffffff

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   }, []);
 
   return (
-    <div className="min-h-screen bg-white">
+    <div className="min-h-screen bg-[#ffffff]">
       <HeroSection />
       <IntroSection />
       <QuizSection />
