2d0707da5d6e17bb6a164c6ec199c95a542cef9e
mariadebahia <maria.zerihoun@student.berghs.se>
2025-05-12 14:26:41 +0200

    Update Netlify configuration and frontend code

--- src/sections/QuizSection.tsx
+++ src/sections/QuizSection.tsx
@@ -6,7 +6,7 @@ import Button from '../components/Button';
 import AnimatedSection from '../components/AnimatedSection';
 import { Dumbbell, Brain, Rocket, TrendingUp, Users } from 'lucide-react';
 
-const QUIZ_ENDPOINT = 'https://script.google.com/macros/s/AKfycbwuSibl18yUHjWLqSn0fm6oBtXNvf8PA7MVISnEpMU0Rqj8xfZcoYkKN36hYi1iLql_/exec';
+const QUIZ_ENDPOINT = '/api/quiz';
 
 const calculateKompetensgapPercent = (answers: Record<string | number, string>): number => {
   const kompetensQuestions = [1, 3, 5, 7, 9]; // Questions related to competency
