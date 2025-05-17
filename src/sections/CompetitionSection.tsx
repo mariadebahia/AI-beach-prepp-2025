133a0141a0980d08e798d642684c82cd69f230d6
mariadebahia <maria.zerihoun@student.berghs.se>
2025-05-17 13:14:52 +0200

    Add useState import to CompetitionSection

--- src/sections/CompetitionSection.tsx
+++ src/sections/CompetitionSection.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../components/Button';
 import AnimatedHeader from '../components/AnimatedHeader';
 import { Trophy } from 'lucide-react';
