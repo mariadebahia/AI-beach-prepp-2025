8aa6bf612bda78793f23e3f5f9a05929f4f9af0b
mariadebahia <maria.zerihoun@student.berghs.se>
2025-05-17 13:57:29 +0200

    Update paragraph font size to em units

--- src/sections/CompetitionSection.tsx
+++ src/sections/CompetitionSection.tsx
@@ -23,7 +23,7 @@ const CompetitionSection: React.FC = () => {
                 KOMMA IGÅNG!
               </h2>
               
-              <p className="text-[18px]">
+              <p className="text-[1.125em]">
                 Vi kommer till din arbetsplats och AI-boostar teamet med grunderna, verktyg och tips för att 
                 komma i AI-form till sommaren (så att ni är redo för hösten). Vi kallar det AI-beach prepp men en svindyr managementkonsult skulle nog kalla det för "get-AI-ready-or-die". Alla som anmäler sig får vårt AI-fitness program för att börja komma igång på egen hand.
               </p>
